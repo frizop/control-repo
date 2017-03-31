0e239d8ac2559ef1171d32eabfb04ae798ba8be8
Nathan Morell <github.com@fap.me>
2017-03-31 15:26:08 -0500

    iis_feature added

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,4 +4,17 @@ class profile::base {
     command  => '[System.Console]::Error.WriteLine("foo")',
     provider => powershell,
   }
+
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+  iis_feature { $iis_features:
+    ensure => present,
+  } ->
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultApplicationPool',
+  }
+
 }
