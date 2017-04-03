5c57cc274fe705f09d2ab4c1cd43f1aa4565b03a
Nathan Morell <github.com@fap.me>
2017-04-03 13:42:25 -0500

    Updates to puppetlabs-iis profile

--- site/profile/manifests/puppetlabsiis.pp
+++ site/profile/manifests/puppetlabsiis.pp
@@ -2,11 +2,11 @@
 #
 #
 class profile::puppetlabsiis {
-  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  # $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 
-  iis_feature { $iis_features:
-    ensure => present,
-  } ->
+  # iis_feature { $iis_features:
+  #   ensure => present,
+  # } ->
 
   iis_site { 'minimal':
     ensure          => 'started',
