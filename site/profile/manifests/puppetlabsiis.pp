285492dd988b1488fdabea2845b589dfa2cd5532
Nathan Morell <github.com@fap.me>
2017-04-03 14:11:37 -0500

    more testing

--- site/profile/manifests/puppetlabsiis.pp
+++ site/profile/manifests/puppetlabsiis.pp
@@ -2,16 +2,29 @@
 #
 #
 class profile::puppetlabsiis {
-  # $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  $iis_features = ['Web-WebServer']
 
-  # iis_feature { $iis_features:
-  #   ensure => present,
-  # } ->
+  iis_feature { $iis_features:
+    ensure => present,
+  } ->
 
   iis_site { 'minimal':
     ensure          => 'started',
     physicalpath    => 'c:\\inetpub\\minimal',
     applicationpool => 'DefaultApplicationPool',
+  }
+
+  iis_application_pool { 'minimal_site_app_pool':
+    ensure                => 'present',
+    managedpipelinemode   => 'Integrated',
+    managedruntimeversion => 'v4.0',
+    state                 => 'Started'
+  } ->
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'minimal_site_app_pool',
   }
 
 }
