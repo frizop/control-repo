c2ec83030c4aea217f7940d8fc99960505f0182d
Nathan Morell <github.com@fap.me>
2017-04-17 17:29:37 -0500

    Add dotnet install test!

---
+++ site/profile/manifests/dotnet462.pp
@@ -0,0 +1,15 @@
+# Class: profile::dotnet462
+#
+#
+class profile::dotnet462 {
+
+  if $::dotnet_version != '4.6.2' {
+
+    exec { 'install-dotnet462':
+      command  => 'Start-Process -Wait -FilePath C:\\Users\\vagrant\\Downloads\\NDP462-KB3151800-x86-x64-AllOS-ENU.exe -ArgumentList /q, /norestart'
+      provider => powershell,
+    }
+
+  }
+
+}
