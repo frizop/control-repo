ac9e0fbc949f5875f26b9f036a8a643479c1755a
Nathan Morell <github.com@fap.me>
2017-04-25 17:58:33 -0500

    dotnet 462 retrying the /norestart option with the reboot module

--- site/profile/manifests/dotnet462.pp
+++ site/profile/manifests/dotnet462.pp
@@ -6,7 +6,7 @@ class profile::dotnet462 {
   if $::dotnet_version != '4.6.2' {
 
     exec { 'install-dotnet462':
-      command  => 'Start-Process -Wait -FilePath C:\\vagrant\\files\\NDP462-KB3151800-x86-x64-AllOS-ENU.exe -ArgumentList /q',
+      command  => 'Start-Process -Wait -FilePath C:\\vagrant\\files\\NDP462-KB3151800-x86-x64-AllOS-ENU.exe -ArgumentList /q, /norestart',
       provider => powershell,
     }
 
