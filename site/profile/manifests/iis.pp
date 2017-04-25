febc139b6995e0139e14659f9c9c6135eeea35fe
Nathan Morell <github.com@fap.me>
2017-04-25 15:15:36 -0500

    Adding some more logic to profile::iis
    - Changes attempt to get first run correct
    - Depending on iis_site being deleted and then cleaning up resouces

--- site/profile/manifests/iis.pp
+++ site/profile/manifests/iis.pp
@@ -41,12 +41,18 @@ class profile::iis (
     ensure   => present,
     require  => Windowsfeature['Web-Server'],
     bindings => ['http/*:80:'],
+    require  => Iis_app['Default Web Site/'],
   }
 
   # We're going to remove the default web site, as it is already bound to port-80
   iis_app { 'Default Web Site/':
     ensure  => absent,
     require => Windowsfeature['Web-WebServer'],
+  }
+
+  iis_apppool { 'DefaultAppPool':
+    ensure  => absent,
+    require => Iis_app['Default Web Site/'],
   }
 
   iis_app { "${appname}/":
