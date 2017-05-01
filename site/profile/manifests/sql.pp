4fd9710e4788d6e48e06b6f5f7bb2110ec9b1c9b
Nathan Morell <github.com@fap.me>
2017-05-01 18:06:17 -0500

    attempting to get this working

---
+++ site/profile/manifests/sql.pp
@@ -0,0 +1,66 @@
+# Simple SQL Profile to install MSSQL based on share
+class profile::sql (
+
+  String $collation = pick($::puppet_vra_property.dig('Albertsons.COLLATION'), 'SQL_Latin1_General_CP1_CI_AS'),
+  String $sqlport   = pick($::puppet_vra_property.dig('Albertson.SqlServer.PortNumber'), '52065'),
+  String $sqluser   = pick($::puppet_vra_property.dig('Albertson.SqlServer.UserName'), 'loggingUser'),
+  String $sqlpw     = pick($::puppet_vra_property.dig('Albertson.SqlServer.Password'), 'Pupp3t1@'),
+
+) {
+
+  notify { 'message':
+    message => $::os['family'],
+  }
+
+  notify { 'roleclass':
+    message => $::puppet_vra_properties['Puppet.RoleClass'],
+  }
+
+  notify { 'sqlport':
+    message => $::puppet_vra_properties['Albertson.SqlServer.PortNumber'],
+  }
+
+  sqlserver_instance { 'MSSQLSERVER':
+    features              => ['SQLEngine', 'Replication', 'FullText'],
+    source                => '\\\\pcntapnas01.safeway.com\\atlas\\paas\\Database\\SQL2014ENT',
+    # agt_svc_account       => '',
+    # agt_svc_password      => '',
+    # sql_svc_account       => '',
+    # sql_svc_password      => '',
+    sql_sysadmin_accounts => ['myuser'],
+    security_mode         => 'SQL',
+    sa_pwd                => 'p@ssw0rd!!',
+    install_switches      => {
+      'TCPENABLED'   => 1,
+      'SQLCOLLATION' => $collation,
+      # 'INSTANCEDIR'         => 'C:\\MSSQL11.MSSQLSERVER',
+      # 'INSTALLSHAREDDIR'    => 'C:\\Program Files\\Microsoft SQL Server',
+      # 'INSTALLSHAREDWOWDIR' => 'C:\\Program Files\\Microsoft SQL Server WOW',
+      # 'INSTALLSQLDATADIR'   => 'C:\\MSSQL11.MSSQLSERVER',
+      # 'SQLUSERDBLOGDIR'     => 'C:\\MSSQL11.MSSQLSERVER',
+      # 'SQLTEMPDBDIR'        => 'C:\\MSSQL11.MSSQLSERVER',
+    }
+  }
+
+  sqlserver_features { 'Generic Features':
+    source   => '\\\\pcntapnas01\\atlas\\paas\\Database\\SQL2014ENT',
+    features => ['Tools'],
+    # is_svc_account  => '',
+    # is_svc_password => '',
+  }
+
+  # Example of creating the user based on params
+  sqlserver::login{ $sqluser:
+    instance => 'MSSQLSERVER',
+    password => $sqlpw,
+  }
+
+  # Default Instance Registery setting for TCP/IP Port
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\TCP\IPAll
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\TCP\IPAll':
+    ensure => present,
+    type   => string,
+    data   => 'The Puppet Agent service periodically manages your configuration',
+  }
+
+}
