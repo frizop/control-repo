d301f43ed7e791fd07dd5889fbdfde33a13f5ef4
Nathan Morell <github.com@fap.me>
2017-05-30 23:47:25 -0500

    More updates

--- site/profile/manifests/vault.pp
+++ site/profile/manifests/vault.pp
@@ -2,5 +2,21 @@
 #
 #
 class profile::vault {
+
+  if $::facts['osfamily'] != 'windows' {
+    package { 'wget':
+      ensure => present,
+    }
+
+    package { 'bunzip':
+      ensure => present,
+    }
+
+    Archive {
+      provider => 'wget',
+      require  => Package['wget', 'bunzip'],
+    }
+  }
+  
   include vault
 }
