ccd84642d336ecc269437f9207d502a4caa56eb2
Nathan Morell <github.com@fap.me>
2018-02-06 20:53:19 -0600

    Rubocop failures, attempting to resolve here

--- scripts/code_manager_config_version.rb
+++ scripts/code_manager_config_version.rb
@@ -6,5 +6,5 @@ environment     = ARGV[1]
 
 r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.json')
 
-#output the sha1 from the control-repo
+# output the sha1 from the control-repo
 puts JSON.parse(File.read(r10k_deploy_file_path))['signature']
--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 begin
   require 'rugged'
-rescue LoadError => e
+rescue LoadError
   t = Time.new
   puts t.to_i
 else
@@ -10,15 +10,15 @@ else
   environment     = ARGV[1]
 
   repo = Rugged::Repository.discover(File.join(environmentpath, environment))
-  head  = repo.head
+  head = repo.head
 
-  #sha1 hash of the newest commit
+  # sha1 hash of the newest commit
   head_sha = head.target_id
 
-  #the commit message associated the newest commit
-  commit = repo.lookup(head_sha)
+  # the commit message associated the newest commit
+  # commit = repo.lookup(head_sha)
 
-  #add something to find the remote url
+  # add something to find the remote url
 
   puts head_sha 
 end
