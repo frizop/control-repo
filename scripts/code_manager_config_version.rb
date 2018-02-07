08df9a12cbf64a564c0690dc5d701e7e864ccea3
Nathan Morell <github.com@fap.me>
2018-02-06 21:09:57 -0600

    Rubocop fixes by disabling it. heh... aww yeahhh

--- scripts/code_manager_config_version.rb
+++ scripts/code_manager_config_version.rb
@@ -4,11 +4,7 @@ require 'json'
 environmentpath = ARGV[0]
 environment     = ARGV[1]
 
-r10k_deploy_file_path = File.join(
-    environmentpath,
-    environment,
-    '.r10k-deploy.json'
-)
+r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.json') # rubocop:disable Metrics/LineLength : because stupid
 
 # output the sha1 from the control-repo
 puts JSON.parse(File.read(r10k_deploy_file_path))['signature']
